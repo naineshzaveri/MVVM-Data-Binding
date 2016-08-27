4df0e670338890d3d2151d3833a9b8c0bfb0049e
naineshzaveri123 <naineshzaveri@gmail.com>
2016-08-27 06:45:23 +0530

    code cleanup

--- app/src/main/java/ocean/movie/com/movieocean/adapters/MovieListAdapter.java
+++ app/src/main/java/ocean/movie/com/movieocean/adapters/MovieListAdapter.java
@@ -22,20 +22,6 @@ public class MovieListAdapter  extends RecyclerView.Adapter<MovieListAdapter.Bin
     private ocean.movie.com.movieocean.databinding.RowMoviePosterBinding binding;
     private android.view.View.OnClickListener onClickListener;
 
-    public static class MoviePosterViewHolder extends RecyclerView.ViewHolder {
-        private android.databinding.ViewDataBinding mViewDataBinding;
-
-        public MoviePosterViewHolder( android.databinding.ViewDataBinding viewDataBinding) {
-            super(viewDataBinding.getRoot());
-
-            mViewDataBinding = viewDataBinding;
-            mViewDataBinding.executePendingBindings();
-        }
-        public android.databinding.ViewDataBinding getViewDataBinding() {
-            return mViewDataBinding;
-        }
-
-    }
 
     public MovieListAdapter(android.content.Context context, ArrayList<MovieModel> arrMovies,android.view.View.OnClickListener onClickListener) {
         super();
@@ -44,22 +30,6 @@ public class MovieListAdapter  extends RecyclerView.Adapter<MovieListAdapter.Bin
         this.onClickListener = onClickListener;
     }
 
-   /* @Override
-    public void onBindViewHolder(final android.support.v7.widget.RecyclerView.ViewHolder viewHolder, int position) {
-
-        MovieListAdapter.MoviePosterViewHolder holder = (MovieListAdapter.MoviePosterViewHolder) viewHolder;
-        MovieModel movieModel = arrMovies.get(position);
-
-        android.databinding.ViewDataBinding viewDataBinding = holder.getViewDataBinding();
-
-        viewDataBinding.setVariable(ocean.movie.com.movieocean.BR.movieData, movieModel);
-        viewDataBinding.setV
-       *//* ocean.movie.com.movieocean.utils.BitmapUtils.setUrlImageUsingPicasso(mContext, movieModel.getPosters().getOriginal(), holder.ivMoviePoster, holder.progressBar);
-        holder.txtMovieName.setText(movieModel.getTitle());
-        holder.txtReleaseDate.setText(movieModel.getRelease_dates().getTheater());
-        holder.txtUserRatings.setText("" + movieModel.getRatings().getAudience_score());
-        holder.cvContent.setOnClickListener(new NextActivityListener(movieModel));*//*
-    }*/
     @Override
     public void onBindViewHolder(BindingHolder holder, int position) {
         ocean.movie.com.movieocean.databinding.RowMoviePosterBinding postBinding = holder.binding;
@@ -84,30 +54,6 @@ public class MovieListAdapter  extends RecyclerView.Adapter<MovieListAdapter.Bin
                 parent,
                 false);
         return new BindingHolder(postBinding);
-    }
-   /* @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        android.databinding.ViewDataBinding binding = android.databinding.DataBindingUtil.inflate(LayoutInflater.from(viewGroup.getContext()), R.layout.row_movie_poster, viewGroup, false);
-
-        //View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_movie_poster, viewGroup, false);
-        return new MovieListAdapter.MoviePosterViewHolder(binding);
-    }*/
-
-    class NextActivityListener implements android.view.View.OnClickListener {
-        MovieModel movieModel;
-
-        NextActivityListener(MovieModel movieModel) {
-            this.movieModel = movieModel;
-        }
-
-        @Override
-        public void onClick(android.view.View v) {
-            android.content.Intent intent = new android.content.Intent(mContext, ocean.movie.com.movieocean.activity.MovieDetailActivity.class);
-            android.os.Bundle bundle = new android.os.Bundle();
-            bundle.putSerializable(ocean.movie.com.movieocean.utils.BundleKeys.KEY_MOVIE, movieModel);
-            intent.putExtras(bundle);
-            mContext.startActivity(intent);
-        }
     }
 
     public static class BindingHolder extends android.support.v7.widget.RecyclerView.ViewHolder {
